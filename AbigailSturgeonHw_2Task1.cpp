49e39e89676ae870c6b59d27f46529f199dcbff6
abigailsturgeon <31452283+abigailsturgeon@users.noreply.github.com>
2017-09-19 13:00:57 -0600

    First commit

---
+++ AbigailSturgeonHw_2Task1.cpp
@@ -0,0 +1,21 @@
+// File: AbigailSturgeonHw_2Task1
+// Created by Abigail Sturgeon on 9/19/2017 for CS1410.
+// Copyright (c) 2017 WSU
+
+#include <iostream>
+
+using namespace std;
+
+// Constants
+
+// Prototypes
+
+// Main Program Program
+
+int main(void) {
+
+    return 0;
+
+}
+
+// Function Definitions
